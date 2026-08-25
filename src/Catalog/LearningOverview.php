3564c1e516d55e76882025763703ec8cc0ed4e65
Lars Moelleken <lars@moelleken.org>
2026-08-26 01:38:24 +0200

    fix(catalog): expose bounded recent history ids

--- src/Catalog/LearningOverview.php
+++ src/Catalog/LearningOverview.php
@@ -13,6 +13,8 @@ final readonly class LearningOverview
      * @param list<string> $findingAttentionIds
      * @param list<string> $proposalAttentionIds
      * @param list<string> $recentDurableGuidanceIds
+     * @param list<string> $recentFindingIds
+     * @param list<string> $recentProposalIds
      */
     public function __construct(
         public array $findingCounts,
@@ -21,6 +23,8 @@ final readonly class LearningOverview
         public array $findingAttentionIds,
         public array $proposalAttentionIds,
         public array $recentDurableGuidanceIds,
+        public array $recentFindingIds = [],
+        public array $recentProposalIds = [],
     ) {
     }
 }
