e49b1854756e8a7cfbb59504fe9aecc2b9ea887c
Lars Moelleken <lars@moelleken.org>
2026-08-31 22:37:59 +0200

    feat(learning): expose LearningNote read projection

---
+++ src/LearningNoteProjection.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace voku\AgentLearning;
+
+final readonly class LearningNoteProjection
+{
+    /**
+     * @param list<string> $scope
+     * @param list<string> $tags
+     * @param list<string> $sourceFindings
+     */
+    public function __construct(
+        public string $id,
+        public string $patternKey,
+        public LearningNoteStatus $status,
+        public array $scope,
+        public array $tags,
+        public array $sourceFindings,
+        public LearningNoteContent $content,
+        public string $sourceDigest,
+    ) {
+    }
+}
